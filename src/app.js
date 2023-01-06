fbf2ab1a0fb223e43692f73a03b3c068723a76e7
Jose Alejandro Gonzalez Ortiz <70743629+JoseGonzalez-git@users.noreply.github.com>
2023-01-06 09:44:13 -0500

    update : add cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 //CONFIG SERVER
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 // Routes
 import IndexRouter from "./routes/index.routes";
 import routerMenu from "./routes/menu.routes";
@@ -12,11 +13,18 @@ import userRouter from "./routes/user.routes";
 const app = express();
 createdRoles(); //solo se ejecuta si no hay roles.
 //variable- valor
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 5045);
 app.set("pkg", pkg);
 
 app.use(morgan("dev"));
 app.use(express.json());
+
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 //Routes
 app.use("/api/v1", IndexRouter);
 app.use("/api/v1/menu", routerMenu);
